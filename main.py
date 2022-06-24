5bf0bb7486f724539ea94840e6640817890c930e
jud-ie19 <owuorakinyijudith@gmail.com>
2022-06-24 15:53:01 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,45 @@
+from pkg_resources import ensure_directory
+
+
+a="I am Akirachix"
+b=a.split()
+start=0
+end=len(b)-1
+while start<end:
+    b[start],b[end]=b[end],b[start]
+    start+=1
+    end-=1
+    str=" "
+print(str.join(b))
+
+def name(x):
+    z=x.split()
+    start=0
+    end=len(z)-1
+    while start<end:
+       z[start],z[end]=z[end],z[start]
+    start+=1
+    end-=1
+    str=""
+    str.join(z)
+    if (str==z):
+        print("its a palindrome")
+    else:
+        print("its not a palindrome")
+name("mum")
+
+    
+
+
+
+
+
+
+
+       
+       
+    
+      
+    
+
+
